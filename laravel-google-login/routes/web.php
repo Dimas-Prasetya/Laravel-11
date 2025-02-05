772842e55ffb6b002cd01f73cd53a6f5c10a83f7
dimas-prasetya <prasetya.dimas95@gmail.com>
2025-02-05 08:56:37 +0700

    add new route for oauth

--- laravel-google-login/routes/web.php
+++ laravel-google-login/routes/web.php
@@ -15,3 +15,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('oauth/google', [\App\Http\Controllers\OauthController::class, 'redirectToProvider'])->name('oauth.google');
+Route::get('oauth/google/callback', [\App\Http\Controllers\OauthController::class, 'handleProviderCallback'])->name('oauth.google.callback');
